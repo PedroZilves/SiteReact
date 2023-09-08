89dc7236562d8c3964924befb8c5c18835365b66
MatheusCosta616 <rm551157@fiap.com.br>
2023-09-08 18:28:50 -0300

    odeio react 2023

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
 import './Home.css'
 import Cabecalho from "./componentes/cabecalho"
+import Rodape from './componentes/rodape'
 
 export default function Home() {
 
   return (
     <>
     <div id="grid-container">
-      <div id="grid-iten">
+      <div id="grid-item1">
         <Cabecalho/>
+        </div>
+        <div id="grid-item2">
+        dasdasdasdasd
+        </div>
+        <div id="grid-item3">
+          dasdasdasdasdas
+        </div>
+        <div class="rodape">
+          <Rodape/>
         </div>
       </div>
     </>
---
+++ src/componentes/rodape.jsx
@@ -0,0 +1,11 @@
+import './rodape.css'
+
+export default function Rodape(){
+    return(
+        <footer className="rodape">
+            <div>  
+                <p><strong>&copy; 2023 - Todos os direitos reservados Dream Clean</strong></p>
+            </div>
+        </footer>
+    )
+}
